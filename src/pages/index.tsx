baf8a2372f159aaf491b1cd4bf85b9f911dcaa38
Adalberto Shindy <adalberto.shindy@hotmail.com>
2021-04-22 02:20:05 -0300

    Criado página home, roteamento no Next.js, página episódio, páginas estáticas e dinâmicas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,156 @@
 //SPA - Single Page Application. Os dados são carregados apenas no momento que o usuário acessa a página 
 //(problema de indexação, no google por exemplo)
-//SSG - Server Side Generation. A página já estará carregada no servidor next, assim quando o usuário acessa-la, já estará
+//SSR - Server-Side Rendering. A página já estará carregada no servidor next, assim quando o usuário acessa-la, já estará
 //carregada, o problema é que ela consome muitos recursos, pois são várias instâncias sendo carregadas para cada usuário,
 //dessa forma, sempre estará atualizada com as novidades (caso a página seja alterada).
 //SSG - Static Side Generation. A página já vai estar carregada no browser quando o cliente acessar, mas essa mesma
-//instância será carregada para todos os usuários que acessarem a página (performance), dessa forma é uma página estática, como sabe-se
-//que este projeto não possui muitas atualizações constantes, este método é vantajoso pois não consome muitos recursos,
-//a página apenas será atualizada no tempo que o programador estipular.
+//instância será carregada para todos os usuários que acessarem a página (performance), dessa forma é uma página estática, 
+//como sabe-se que este projeto não possui muitas atualizações constantes, este método é vantajoso pois não consome muitos,
+//recursos a página apenas será atualizada no tempo que o programador estipular.
 
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link'; //Biblioteca que otimiza a aplicação com chamadas de links (a página não é recarregada do zero)
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+import styles from './home.module.scss';
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+}
+
+type HomeProps = {
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <div>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
-    </div>
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio" />
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button>
+                      <img src="/play-green.svg" alt="Tocar episódio" />
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div >
   )
 }
 
 //SSG
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12, //carrega no máximo 12 episódios da API na página
+      _sort: 'published_at', //ordena na ordem da data de publicação
+      _order: 'desc' //ordena na ordem decrescente
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      url: episode.file.url,
+    };
+  })
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
 
   return {
     props: {
-      episodes: data,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8, //tempo estipulado de atualização da página
   }
